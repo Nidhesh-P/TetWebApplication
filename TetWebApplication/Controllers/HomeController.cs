d5c8187cec04193358c838bf018777680fa34150
nidhesh pandey <nidhesh@live.com>
2022-03-16 09:06:58 +0530

    jquery

--- TetWebApplication/Controllers/HomeController.cs
+++ TetWebApplication/Controllers/HomeController.cs
@@ -7,11 +7,7 @@ using System.Web.Mvc;
 namespace TetWebApplication.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+    {     
 
         public ActionResult About()
         {
@@ -24,6 +20,10 @@ namespace TetWebApplication.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult Test()
+        {
             return View();
         }
     }
